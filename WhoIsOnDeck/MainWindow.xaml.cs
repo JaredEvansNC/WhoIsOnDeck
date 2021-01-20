2187a1970c70c53d36a3b11a6146e751e91ee812
Jared Evans <jaredevans1985@gmail.com>
2021-01-20 16:57:33 -0700

    Add project files.

---
+++ WhoIsOnDeck/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WhoIsOnDeck
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public StudentSorter Sorter;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			// Try to create a new sorter object, if there are any IO exceptions, print out an error message
+			try
+			{
+				Sorter = new StudentSorter(".\\classnames.txt");
+			}
+			catch(IOException e)
+			{
+				ErrorBlock.Text =	"There was an error, and the students couldn't be loaded.\n" +
+									"Make sure there is a classnames.txt file in the same directory as the .exe, with names separated by commas.\n\n" +
+									"ERROR: " + e.Message;
+			}
+		}
+
+		// If a student's name is click, mark them off as having answered, and get a new set of students
+		private void StudentNameMouseDown(object sender, MouseButtonEventArgs e)
+		{
+			// Don't do this if there's no sorter
+			if (Sorter != null) { 
+
+				string textBlockName = ((TextBlock)sender).Text;
+
+				// If there's a name here, mark it as used
+				if (!textBlockName.Equals("")) {
+					Sorter.MarkNameAsUsed(textBlockName);
+				}
+
+				// Get a new set of students
+				GetNewStudentsOnDeck();
+			}
+		}
+
+		// Retrieves a new set of students from the sorter and displays their names on screen
+		private void GetNewStudentsOnDeck()
+		{
+			// Fill all three labels with new random students
+			string[] studentsOnDeck = Sorter.GetStudentsOnDeck();
+			StudentOne.Text = studentsOnDeck[0];
+			StudentTwo.Text = studentsOnDeck[1];
+			StudentThree.Text = studentsOnDeck[2];
+		}
+
+		private void NewSetButtonMouseDown(object sender, RoutedEventArgs e)
+		{
+			// Don't do this if there's no sorter
+			if (Sorter != null)
+			{
+				GetNewStudentsOnDeck();
+			}
+		}
+	}
+}
---
+++ WhoIsOnDeck/StudentSorter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace WhoIsOnDeck
+{
+	public class StudentSorter
+	{
+		// Students available to be shown
+		private List<string> UnusedStudents;
+		// Used temporarily during the selection process and cleared afterwards
+		private List<string> StudentsSelectedToBeOnDeck = new List<string>();
+		// Students who have already answered a question, shuffled back into unused when unused is too low
+		private List<string> UsedStudents = new List<string>();
+		// The current set of on0deck students
+		private string[] StudentsOnDeck = new string[3];
+
+		public StudentSorter(string nameFilePath) 
+		{
+			// Read names from file in same directory
+			// Put in unused students list
+			string allNames = File.ReadAllText(nameFilePath);
+			UnusedStudents = new List<string>(allNames.Split(','));
+
+		}
+
+		// Returns an array containing a new set of on-deck students, chosen randomly
+		public string[] GetStudentsOnDeck()
+		{
+			// Get three unused students to be on deck
+			for(int i = 0; i < 3; i++)
+			{
+				StudentsOnDeck[i] = GetRandomUnusedStudent();
+			}
+
+			// Now that the students have been selected, move the selected students back into the unused list
+			UnusedStudents.AddRange(StudentsSelectedToBeOnDeck);
+			StudentsSelectedToBeOnDeck.Clear();
+
+			// Return the array
+			return StudentsOnDeck;
+		}
+
+		// Get's a random student from the unused students list
+		private string GetRandomUnusedStudent()
+		{
+			// Refill the unused students list if we run out
+			if(UnusedStudents.Count <= 0)
+			{
+				UnusedStudents.AddRange(UsedStudents);
+				UsedStudents.Clear();
+			}
+
+			// Get a random unused student
+			int randIndex = new Random().Next(0, UnusedStudents.Count);
+			// Store student name
+			string studentName = UnusedStudents[randIndex];
+			// Add this name to the selected list
+			StudentsSelectedToBeOnDeck.Add(studentName);
+			// Remove from the Unused list
+			UnusedStudents.Remove(studentName);
+			// Return the name at that index
+			return studentName;
+		}
+
+		// Called to mark a student's name as used when the answer a question
+		public void MarkNameAsUsed(string name)
+		{
+			UsedStudents.Add(name);
+			UnusedStudents.Remove(name);
+		}
+	}
+}
